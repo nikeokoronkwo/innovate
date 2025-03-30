c0bd2fcefed16fd2de0d18e53838af036d7190a6
Nike Okoronkwo <nikechukwu@gmail.com>
2025-03-30 01:54:33 -0500

    added page feature

--- frontend/pages/supplies/track/[id].vue
+++ frontend/pages/supplies/track/[id].vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+const route = useRoute();
+
+
+</script>
+
 <template>
   <div>
     <Panel>
-      
+      <template #header>
+
+      </template>
+      <div>
+
+      </div>
     </Panel>
   </div>
 </template>
