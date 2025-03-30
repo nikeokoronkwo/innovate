4673b42898043d693527ace1b6c5fd4c893bd4e9
Bright <bright.andohh@gmail.com>
2025-03-29 19:07:49 -0500

    DB migrations && registration

--- backend/innovate/models.py
+++ backend/innovate/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_groups',  # Unique name for reverse accessor
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_permissions',  # Unique name for reverse accessor
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    remote = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15,validators=[RegexValidator(r'^\d{10}$', 'Enter a valid 10-digit phone number.')])
+    location = models.CharField(max_length=255)
